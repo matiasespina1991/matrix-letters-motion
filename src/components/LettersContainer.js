ea999cfb374e224fe5fc58a15b26cbacdccb42cb
Matias Espina <matiasespina1991@gmail.com>
2021-07-10 20:46:50 +0200

    monospace font

--- src/components/LettersContainer.js
+++ src/components/LettersContainer.js
@@ -1,6 +1,6 @@
 function LettersContainer() {
 
-    let lettersString = "ABCDEFGHIJKLMNOPQRST/UVWXYZ123456789101112";
+    let lettersString = "ABCDEFGHIJ/KLMNOPQRST/UVWXYZ1234";
     const lettersBreakSplit = lettersString.split("/");
 
     return (
@@ -9,12 +9,12 @@ function LettersContainer() {
                 {
                     lettersBreakSplit.map(
                         function(line) {
-                                return <div class="letters-line">
-                                            {line.split('').map(
-                                                                    (letter) => <div class="letter">{letter}</div>
-                                                                )
-                                            }
-                                        </div>
+                            return <div class="letters-line">
+                                {line.split('').map(
+                                                    (letter) => <div class="letter">{letter}</div>
+                                                )
+                                }
+                            </div>
                         } 
                     )
                 }
